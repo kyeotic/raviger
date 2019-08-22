f0598331677a5b784dbef52225006de0113c3c67
Tim Kye <Timothy.Kye@nike.com>
2019-08-22 11:18:19 -0700

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,5 @@ module.exports = {
   extends: ['@kyeotic/eslint-config'],
   rules: {
     'react/display-name': 0
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  env: {
-    browser: true
-  },
-  overrides: [
-    {
-      files: ['**/*.spec.js'],
-      env: {
-        jest: true // now **/*.test.js files' env has both es6 *and* jest
-      },
-      // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
-      // "extends": ["plugin:jest/recommended"]
-      plugins: ['jest'],
-      rules: {
-        'jest/no-disabled-tests': 'warn',
-        'jest/no-focused-tests': 'error',
-        'jest/no-identical-title': 'error',
-        'jest/prefer-to-have-length': 'warn',
-        'jest/valid-expect': 'error',
-        'no-console': 0
-      }
-    }
-  ]
+  }
 }
