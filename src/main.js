aaa96c2fe8fa27872c4e4e6cc72ef9b2532a90d5
Tim Kye <Timothy.Kye@nike.com>
2019-09-02 13:59:36 -0700

    split path into module

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-export { useRoutes, usePath, useQueryParams } from './router.js'
+export { useRoutes, useQueryParams } from './router.js'
 export { useRedirect } from './redirect.js'
 export { default as Link } from './Link.js'
 export { navigate } from './navigate.js'
+export { usePath, usePopState } from './path.js'
