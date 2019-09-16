9f5bf64a521e550a9609abfb2ef3a813514793d9
Liam Dyer <saghendev@gmail.com>
2019-09-16 14:10:40 -0400

    Check if url is not a string

--- src/navigate.js
+++ src/navigate.js
@@ -1,6 +1,6 @@
 export function navigate(url, replaceOrQuery = false, replace = false) {
-  if (typeof url === 'object') {
-    throw new Error('"url" must be a string, was provided an object.')
+  if (typeof url !== 'string') {
+    throw new Error(`"url" must be a string, was provided a(n) ${typeof url}.`)
   }
   if (typeof replaceOrQuery === 'object') {
     url += '?' + new URLSearchParams(replaceOrQuery).toString()
